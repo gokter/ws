9755acd0863600b149c7c6a7db5e0e8009792f87
Flycoding <Flycoding@yeah.net>
2014-09-13 21:29:06 +0800

    add authentication interceptor.
    Signed-off-by: Flycoding <Flycoding@yeah.net>

--- ws.client/src/main/java/com/flyingh/ws/client/Demo.java
+++ ws.client/src/main/java/com/flyingh/ws/client/Demo.java
@@ -1,9 +1,20 @@
 package com.flyingh.ws.client;
 
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.apache.cxf.binding.soap.SoapMessage;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.frontend.ClientProxy;
-import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.headers.Header;
+import org.apache.cxf.helpers.DOMUtils;
+import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import com.flyingh.ws.server.Book;
 import com.flyingh.ws.server.Books;
@@ -15,7 +26,23 @@ public class Demo {
 		final com.flyingh.ws.server.HelloWorldService helloWorldServiceImplPort = new HelloWorldService().getHelloWorldServiceImplPort();
 
 		final Client client = ClientProxy.getClient(helloWorldServiceImplPort);
-		client.getInInterceptors().add(new LoggingInInterceptor());
+		// client.getInInterceptors().add(new LoggingInInterceptor());
+		client.getOutInterceptors().add(new AbstractPhaseInterceptor<SoapMessage>(Phase.PREPARE_SEND) {
+
+			@Override
+			public void handleMessage(SoapMessage message) throws Fault {
+				final List<Header> headers = message.getHeaders();
+				final Document document = DOMUtils.createDocument();
+				final Element authenticationElement = document.createElement("authentication");
+				final Element usernameElement = document.createElement("username");
+				final Element passwordElement = document.createElement("password");
+				usernameElement.setTextContent("flyingh");
+				passwordElement.setTextContent("password");
+				authenticationElement.appendChild(usernameElement);
+				authenticationElement.appendChild(passwordElement);
+				headers.add(0, new Header(new QName("flyingh"), authenticationElement));
+			}
+		});
 		client.getOutInterceptors().add(new LoggingOutInterceptor());
 
 		System.out.println(helloWorldServiceImplPort.say("Flyingh"));
--- ws.server/src/main/java/com/flyingh/ws/demo/Demo.java
+++ ws.server/src/main/java/com/flyingh/ws/demo/Demo.java
@@ -1,10 +1,19 @@
 package com.flyingh.ws.demo;
 
+import java.util.List;
+
 import javax.xml.ws.Endpoint;
 
+import org.apache.cxf.binding.soap.SoapMessage;
+import org.apache.cxf.common.util.CollectionUtils;
+import org.apache.cxf.headers.Header;
+import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.interceptor.LoggingInInterceptor;
-import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 import com.flyingh.ws.server.impl.HelloWorldServiceImpl;
 
@@ -12,6 +21,34 @@ public class Demo {
 	public static void main(String[] args) {
 		final EndpointImpl endpointImpl = (EndpointImpl) Endpoint.publish("http://localhost:8899/flyingh", new HelloWorldServiceImpl());
 		endpointImpl.getInInterceptors().add(new LoggingInInterceptor());
-		endpointImpl.getOutInterceptors().add(new LoggingOutInterceptor());
+		endpointImpl.getInInterceptors().add(new AbstractPhaseInterceptor<SoapMessage>(Phase.PRE_INVOKE) {
+
+			@Override
+			public void handleMessage(SoapMessage message) throws Fault {
+				System.out.println("-----------------------------------------------" + message);
+				final List<Header> headers = message.getHeaders();
+				if (CollectionUtils.isEmpty(headers)) {
+					throw new Fault(new IllegalArgumentException("No headers"));
+				}
+				for (final Header header : headers) {
+					final Element element = (Element) header.getObject();
+					final NodeList usernames = element.getElementsByTagName("username");
+					final NodeList passwords = element.getElementsByTagName("password");
+					if (usernames == null || passwords == null) {
+						continue;
+					}
+					if (usernames.getLength() != 1 || passwords.getLength() != 1) {
+						throw new Fault(new IllegalArgumentException("only needs a username and password"));
+					}
+					final String username = usernames.item(0).getTextContent();
+					final String password = passwords.item(0).getTextContent();
+					if ("flyingh".equals(username) && "password".equals(password)) {
+						return;
+					}
+					throw new Fault(new IllegalArgumentException("username or password is wrong"));
+				}
+				throw new Fault(new IllegalArgumentException("no username or password"));
+			}
+		});
 	}
 }
